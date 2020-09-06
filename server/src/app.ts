69de12d47801652accc1c3f2cd71576c51646dba
Harry <haihanchen88@gmail.com>
2020-09-05 22:02:20 -0400

    merged

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,14 +2,14 @@ import express, { Express } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import reviewRoutes from "./routes";
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 
 const app: Express = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 dotenv.config();
 
 const PORT: string | number = process.env.PORT || 4000;
-const ipaddress = '192.168.0.16'
+const ipaddress = "192.168.0.16";
 
 app.use(cors());
 app.use(reviewRoutes);
@@ -26,8 +26,8 @@ mongoose
 	.then(() =>
 		app.listen(4000, ipaddress, function () {
 			// console.log(`Server running on http://localhost:${PORT}`}
-			console.log('Server running')}
-		)
+			console.log("Server running");
+		})
 	)
 	.catch((error) => {
 		throw error;
